266b07ac8a97f64705dc491ecb25112ae6e3f086
fenglinz <fenglin.zhang@live.cn>
2016-10-09 00:04:58 +0800

    完善脚本。

--- Mercurius.Sparrow.Backstage/App_Data/Scripts/表结构导出.sql
+++ Mercurius.Sparrow.Backstage/App_Data/Scripts/表结构导出.sql
@@ -1,23 +1,23 @@
-begin
-  declare @isView int;
-  declare @table nvarchar(50);
-  declare @schema nvarchar(50);
-  declare @tableDescription sql_variant;
+BEGIN
+  DECLARE @isView INT;
+  DECLARE @table NVARCHAR(50);
+  DECLARE @schema NVARCHAR(50);
+  DECLARE @tableDescription SQL_VARIANT;
 
-  declare @name nvarchar(50);
-  declare @dataType nvarchar(50);
-  declare @dataLength int;
-  declare @isPrimaryKey int;
-  declare @isNullable int;
-  declare @isIdentity int;
-  declare @dataDefault nvarchar(50);
-  declare @description sql_variant;
+  DECLARE @name NVARCHAR(50);
+  DECLARE @dataType NVARCHAR(50);
+  DECLARE @dataLength INT;
+  DECLARE @isPrimaryKey INT;
+  DECLARE @isNullable INT;
+  DECLARE @isIdentity INT;
+  DECLARE @dataDefault NVARCHAR(50);
+  DECLARE @description SQL_VARIANT;
 
-  declare @index int;
-  declare @ddl table(id int,source nvarchar(max));
+  DECLARE @index INT;
+  DECLARE @ddl TABLE(id INT, source NVARCHAR(max));
 
   -- 查询所有用户表。
-  DECLARE cur_tables cursor for
+  DECLARE cur_tables CURSOR FOR
     WITH cte AS(
       SELECT
         SCHEMA_NAME(schema_id) AS [Schema],
@@ -36,104 +36,104 @@ begin
       LEFT JOIN sys.extended_properties a ON cte.object_id=a.major_id AND a.minor_id=0
     ORDER BY cte.[schema],cte.Name;
 
-  set @index = 0;
-  open cur_tables;
-  fetch next from cur_tables into @schema, @table, @isView, @tableDescription;
+  SET @index = 0;
+  OPEN cur_tables;
+  FETCH NEXT FROM cur_tables INTO @schema, @table, @isView, @tableDescription;
 
-  while @@FETCH_STATUS=0
-  begin
-    declare @sql nvarchar(3000);
-    declare @columnDescription nvarchar(1500);
+  WHILE @@FETCH_STATUS=0
+  BEGIN
+    DECLARE @sql NVARCHAR(max);
+    DECLARE @columnDescription NVARCHAR(max);
 
     -- 查询表的字段。
-    declare cur_columns cursor for
-    SELECT
-      a.name AS Name
-      ,b.name AS DataType
-      ,COLUMNPROPERTY(a.id, a.name, 'PRECISION') AS DataLength
-      ,CASE WHEN EXISTS(
-        SELECT 1 FROM sysobjects
-        WHERE xtype= 'PK' and name in ( 
-          SELECT
-            name
-          FROM sysindexes
-          WHERE indid in(
+    DECLARE cur_columns CURSOR FOR
+      SELECT
+        a.name AS Name
+        ,UPPER(b.name) AS DataType
+        ,COLUMNPROPERTY(a.id, a.name, 'PRECISION') AS DataLength
+        ,CASE WHEN EXISTS(
+          SELECT 1 FROM sysobjects
+          WHERE xtype= 'PK' and name in ( 
             SELECT
-              indid
-            FROM sysindexkeys
-            WHERE id = a.id AND colid=a.colid
+              name
+            FROM sysindexes
+            WHERE indid in(
+              SELECT
+                indid
+              FROM sysindexkeys
+              WHERE id = a.id AND colid=a.colid
+            )
           )
-        )
-      ) THEN 1 ELSE 0 END AS IsPrimaryKey
-      ,a.IsNullable AS IsNullable
-      ,COLUMNPROPERTY(a.id, a.name, 'IsIdentity') AS IsIdentity
-      ,ISNULL(e.text, '') AS DataDefault
-      ,CASE
-        WHEN g.[value] IS NULL THEN a.Name
-        ELSE g.[value]
-      END AS Description
-    FROM syscolumns a
-    LEFT JOIN systypes b 
-      ON a.xusertype=b.xusertype
-    INNER JOIN sys.objects d 
-      ON a.id=d.object_id AND (d.type='U' OR d.type='V') AND d.name<>'dtproperties'
-    INNER JOIN sys.schemas s
-      ON d.schema_id=s.schema_id
-    LEFT JOIN syscomments e 
-      ON a.cdefault=e.id
-    LEFT JOIN sys.extended_properties g 
-      ON a.id=g.major_id AND a.colid=g.minor_id
-    WHERE s.name=@schema AND d.name=@table
-    ORDER BY a.id, a.colorder;
+        ) THEN 1 ELSE 0 END AS IsPrimaryKey
+        ,a.IsNullable AS IsNullable
+        ,COLUMNPROPERTY(a.id, a.name, 'IsIdentity') AS IsIdentity
+        ,ISNULL(e.text, '') AS DataDefault
+        ,CASE
+          WHEN g.[value] IS NULL THEN a.Name
+          ELSE g.[value]
+        END AS Description
+      FROM syscolumns a
+      LEFT JOIN systypes b 
+        ON a.xusertype=b.xusertype
+      INNER JOIN sys.objects d 
+        ON a.id=d.object_id AND (d.type='U' OR d.type='V') AND d.name<>'dtproperties'
+      INNER JOIN sys.schemas s
+        ON d.schema_id=s.schema_id
+      LEFT JOIN syscomments e 
+        ON a.cdefault=e.id
+      LEFT JOIN sys.extended_properties g 
+        ON a.id=g.major_id AND a.colid=g.minor_id
+      WHERE s.name=@schema AND d.name=@table
+      ORDER BY a.id, a.colorder;
 
-    set @index += 1;
+    SET @index += 1;
 
-    open cur_columns;
-    fetch next from cur_columns into @name, @dataType, @dataLength, @isPrimaryKey, @isNullable, @isIdentity, @dataDefault, @description;
+    OPEN cur_columns;
+    FETCH NEXT FROM cur_columns INTO @name, @dataType, @dataLength, @isPrimaryKey, @isNullable, @isIdentity, @dataDefault, @description;
 
-    set @columnDescription='';
-    set @sql = 'Create Table ['+@schema+'].['+@table+']('+char(10);
+    SET @columnDescription='';
+    SET @sql = 'Create Table ['+@schema+'].['+@table+']('+CHAR(10);
 
-    while @@FETCH_STATUS=0
-    begin
-      set @sql+= '[' + @name + '] '+@dataType;
+    WHILE @@FETCH_STATUS=0
+    BEGIN
+      SET @sql+= '  [' + @name + '] '+@dataType;
 
-      if @dataType in('char', 'nchar', 'varchar', 'nvarchar')
-        set @sql+=' ('+ case @dataLength when -1 then 'MAX' else CAST(@dataLength as nvarchar(50)) end+')';
+      IF @dataType in('char', 'nchar', 'varchar', 'nvarchar')
+        SET @sql+=' ('+ CASE @dataLength WHEN -1 THEN 'MAX' ELSE CAST(@dataLength AS NVARCHAR(50)) END+')';
 
-      if @isPrimaryKey=1 set @sql+=' Primary Key';
-      else
-      begin
-        if @isNullable=1 set @sql+=' NULL';
-        else set @sql+=' NOT NULL';
-      end
+      IF @isPrimaryKey=1 SET @sql+=' Primary Key';
+      ELSE
+      BEGIN
+        IF @isNullable=1 SET @sql+=' NULL';
+        ELSE SET @sql+=' NOT NULL';
+      END
 
-      set @sql+=','+char(10);
+      SET @sql+=','+CHAR(10);
 
-      if @description is not null
-      begin
-        set @columnDescription+='EXEC sys.sp_addextendedproperty ''MS_Description'', '''+ convert(nvarchar(250), @description) + ''' ,''SCHEMA'', '''+@schema+''', ''TABLE'', '''+@table+''', ''COLUMN'', '''+@name+''''+char(10)+'GO'+char(10);
-      end
+      IF @description IS NOT NULL
+      BEGIN
+        SET @columnDescription+='EXEC sys.sp_addextendedproperty ''MS_Description'', '''+ CONVERT(NVARCHAR(250), @description) + ''' ,''SCHEMA'', '''+@schema+''', ''TABLE'', '''+@table+''', ''COLUMN'', '''+@name+''';'+CHAR(10)+'GO'+CHAR(10);
+      END
 
-      fetch next from cur_columns into @name, @dataType, @dataLength, @isPrimaryKey, @isNullable, @isIdentity, @dataDefault, @description;
-    end
-    close cur_columns;
-    deallocate cur_columns;
+      FETCH NEXT FROM cur_columns INTO @name, @dataType, @dataLength, @isPrimaryKey, @isNullable, @isIdentity, @dataDefault, @description;
+    END
+    CLOSE cur_columns;
+    DEALLOCATE cur_columns;
 
-    set @sql=SUBSTRING(@sql, 0, len(@sql)-1)+')'+char(10)+'GO'+char(10);
-    set @sql+='EXEC sys.sp_addextendedproperty ''MS_Description'', '''+ convert(nvarchar(250), @tableDescription) + ''', ''SCHEMA'', '''+@schema+''', ''TABLE'', '''+@table+''', NULL, NULL'+char(10)+'GO'+char(10);
+    SET @sql=SUBSTRING(@sql, 0, len(@sql)-1)+CHAR(10)+');'+CHAR(10)+'GO'+CHAR(10);
+    SET @sql+='EXEC sys.sp_addextendedproperty ''MS_Description'', '''+ convert(nvarchar(250), @tableDescription) + ''', ''SCHEMA'', '''+@schema+''', ''TABLE'', '''+@table+''', NULL, NULL;'+CHAR(10)+'GO'+CHAR(10);
 
-    insert into @ddl values(@index,@sql+@columnDescription);
+    INSERT INTO @ddl VALUES(@index,@sql+@columnDescription);
 
-    set @table=null;
-    set @schema = null;
-    set @tableDescription=null;
+    SET @table=NULL;
+    SET @schema=NULL;
+    SET @tableDescription=NULL;
 
-    fetch next from cur_tables into @schema, @table, @isView, @tableDescription;
-  end
+    FETCH NEXT FROM cur_tables INTO @schema, @table, @isView, @tableDescription;
+  END
 
-  close cur_tables;
-  deallocate cur_tables;
+  CLOSE cur_tables;
+  DEALLOCATE cur_tables;
 
-  select * from @ddl order by id asc;
-end
+  SELECT * FROM @ddl ORDER BY id ASC;
+END
